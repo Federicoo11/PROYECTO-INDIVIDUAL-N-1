8a366a798287761f71cb212fccd33501a907a34a
federico11 <federicooputelli@gmail.com>
2024-05-08 00:48:32 -0300

    FastApi

---
+++ FastApi/main.py
@@ -0,0 +1,35 @@
+from fastapi import FastAPI
+from pandas import pandas as pd
+
+app = FastAPI()
+
+result_df = pd.read_csv("C:/Users/Federico/Desktop/Trabajo Individual Henry/1/FastApi/CSV/UserForGenre")
+df_max_hours_year = pd.read_csv("C:/Users/Federico/Desktop/Trabajo Individual Henry/1/FastApi/CSV/PlayTimeGenre")
+
+@app.get("/")
+def UserForGenre(genre):
+    # Suponiendo que tienes el DataFrame 'df' disponible en este contexto
+    genre_df = result_df[result_df['genres'] == genre]
+    
+    # Encontrar el índice del usuario que más jugó para ese género
+    max_playtime_index = genre_df['playtime_forever'].idxmax()
+    
+    # Obtener el usuario y las horas correspondientes
+    max_playtime_user = genre_df.loc[max_playtime_index, 'user_id']
+    max_playtime_hours = genre_df.loc[max_playtime_index, 'playtime_forever']
+    
+    return max_playtime_user, max_playtime_hours
+
+@app.get("/")
+def PlayTimeGenre(genre):
+    # Filtrar el DataFrame por el género especificado
+    df_genre = df_max_hours_year[df_max_hours_year['genre'] == genre]
+    
+    # Verificar si se encontraron datos para el género especificado
+    if df_genre.empty:
+        return None
+    
+    # Obtener el año correspondiente al género con mayor tiempo jugado
+    año_max_playtime = df_genre.loc[df_genre['playtime_forever'].idxmax(), 'year']
+    
+    return año_max_playtime
